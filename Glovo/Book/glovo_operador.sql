61c5f42c91a16314abfb4859d333c604fc5a5a92
carlosmga8 <arq.carlosmauricioga@gmail.com>
2025-03-19 14:00:13 +0100

    Create glovo_operador.sql

---
+++ Glovo/Book/glovo_operador.sql
@@ -0,0 +1,6 @@
+SELECT * FROM "delta"."central_users_odp"."operators"
+where
+ operator_team = 'Glovo staff'
+--and operator_glovo_email = 'chayma.meziani+operator@glovoapp.com'
+--and operator_id in (129110901,167033812,62811036,169967715)
+and operator_id = 156679576
